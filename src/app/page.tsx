28de372047d9d56c20483110a91eca35f42aeea6
Johnny Ruiz <johnny@ruizops.com>
2023-07-28 14:31:27 -0600

    updated styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,56 @@
-"use client";
+"use client"
 
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { useForm } from "react-hook-form";
-import z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Button } from "@/components/ui/button"
+import z from "zod"
 
 const schema = z.object({
-  time: z.string().nonempty({ message: "Time is required" }),
-  timezone: z.string().nonempty({ message: "Timezone is required" }),
-});
+    time: z.string().nonempty({ message: "Time is required" }),
+    timezone: z.string().nonempty({ message: "Timezone is required" }),
+})
 
 export default function Home() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: zodResolver(schema),
-  });
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: zodResolver(schema),
+    })
 
-  const onSubmit = (data: any) => {
-    console.log(data);
-  };
+    const onSubmit = (data: any) => {
+        console.log(data)
+    }
 
-  return (
-    <main className="p-24">
-      <h1 className="text-5xl">Timely</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <Label>Time</Label>
-          <Input type="datetime-local" {...register("time")} />
-          {errors.time && (
-            <p className="text-red-500">
-              <>{errors.time.message}</>
-            </p>
-          )}
-        </div>
-        <div>
-          <Label>Timezone</Label>
-          <Input type="text" {...register("timezone")} />
-          {errors.timezone && (
-            <p className="text-red-500">
-              <>{errors.timezone.message}</>
-            </p>
-          )}
-        </div>
-        <Button type="submit" className="p-5 my-5 bg-blue-500">
-          Submit
-        </Button>
-      </form>
-    </main>
-  );
+    return (
+        <main className="p-24">
+            <h1 className="text-5xl">Timely</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <Label>Time</Label>
+                    <Input type="datetime-local" {...register("time")} />
+                    {errors.time && (
+                        <p className="text-red-500">
+                            <>{errors.time.message}</>
+                        </p>
+                    )}
+                </div>
+                <div>
+                    <Label>Timezone</Label>
+                    <Input type="text" {...register("timezone")} />
+                    {errors.timezone && (
+                        <p className="text-red-500">
+                            <>{errors.timezone.message}</>
+                        </p>
+                    )}
+                </div>
+                <Button type="submit" className="p-5 my-5 bg-blue-500">
+                    Submit
+                </Button>
+            </form>
+        </main>
+    )
 }
