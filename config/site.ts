4e1359f5f00d1d79a5a8891e1eb1cfff192222da
Johnny Ruiz <johnny@ruizops.com>
2023-07-28 15:41:17 -0600

    updated meta data

---
+++ config/site.ts
@@ -0,0 +1,7 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+    title: "Timely",
+    description: "A web app for converting timezones across the world.",
+    githubUrl: "https://github.com/jonathanruiz/timely",
+}
