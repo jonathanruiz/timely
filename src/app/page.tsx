04eeeb65ccdb959a4494aaeaa6fe6b1ed6fbccf5
Johnny Ruiz <johnny@ruizops.com>
2023-07-28 14:18:48 -0600

    added draft form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,56 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+
+const schema = z.object({
+  time: z.string().nonempty({ message: "Time is required" }),
+  timezone: z.string().nonempty({ message: "Timezone is required" }),
+});
+
 export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="p-24">
       <h1 className="text-5xl">Timely</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <Label>Time</Label>
+          <Input type="datetime-local" {...register("time")} />
+          {errors.time && (
+            <p className="text-red-500">
+              <>{errors.time.message}</>
+            </p>
+          )}
+        </div>
+        <div>
+          <Label>Timezone</Label>
+          <Input type="text" {...register("timezone")} />
+          {errors.timezone && (
+            <p className="text-red-500">
+              <>{errors.timezone.message}</>
+            </p>
+          )}
+        </div>
+        <Button type="submit" className="p-5 my-5 bg-blue-500">
+          Submit
+        </Button>
+      </form>
     </main>
   );
 }
