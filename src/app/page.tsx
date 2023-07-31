c8dfb506cfc722b0f632b6571663305568ba894d
Johnny Ruiz <johnny@ruizops.com>
2023-07-30 18:20:52 -0600

    Display Timezone options (#4)
    * added form and select
    
    * added timezones
    
    * added grouping for each timezone
    
    * fixed width of input for timezone
    
    * fixed grouping
    
    * added line length before wrapping in prettier
    
    * fixed searching
    
    * fixed typo
    
    * fixed styling for time
    
    * renamed alias for timezones
    
    * showed current timezone as default timezone
    
    * created a var for localTimezone
    
    * removed unnecessary import
    
    * added UTC in each timezone
    
    * added UTC to default
    
    * change placeholder
    
    * added margin top for labels
    
    * renamed label
    
    * fixed description
    
    * removed watch function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,45 @@
 "use client"
 
 import { zodResolver } from "@hookform/resolvers/zod"
+import { Check, ChevronsUpDown } from "lucide-react"
 import { useForm } from "react-hook-form"
-import z from "zod"
+import * as z from "zod"
 
+import { IANA_TIMEZONES_GROUPS as groups, IANA_TIMEZONES as timezones } from "@/lib/tz"
+import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandShortcut,
+} from "@/components/ui/command"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
 
 const schema = z.object({
     time: z.string().nonempty({ message: "Time is required" }),
     timezone: z.string().nonempty({ message: "Timezone is required" }),
 })
 
+const localTimezone = {
+    group: "Local",
+    timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+    label: "Local timezone",
+}
+
+// Get Coordinated Universal Timezone
+const utc = {
+    group: "UTC",
+    timezone: "Coordinated Universal Time",
+    label: "Coordinated Universal Time (UTC)",
+}
+
 export default function Home() {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm({
+    const form = useForm({
         resolver: zodResolver(schema),
     })
 
@@ -29,33 +50,130 @@ export default function Home() {
     return (
         <main className="p-24">
             <h1 className="text-5xl text-center">Timely</h1>
-            <form
-                onSubmit={handleSubmit(onSubmit)}
-                autoComplete="off"
-                className="m-auto md:w-1/2 sm:w-2/3"
-            >
-                <div>
-                    <Label>Time</Label>
-                    <Input type="datetime-local" {...register("time")} />
-                    {errors.time && (
-                        <p className="text-red-500">
-                            <>{errors.time.message}</>
-                        </p>
-                    )}
-                </div>
-                <div>
-                    <Label>Timezone</Label>
-                    <Input type="text" {...register("timezone")} />
-                    {errors.timezone && (
-                        <p className="text-red-500">
-                            <>{errors.timezone.message}</>
-                        </p>
-                    )}
-                </div>
-                <Button type="submit" className="p-5 my-5 bg-blue-500">
-                    Submit
-                </Button>
-            </form>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} autoComplete="off" className="m-auto md:w-1/2 sm:w-2/3">
+                    <FormField
+                        control={form.control}
+                        name="time"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel className="mt-4">Date and Time</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        type="datetime-local"
+                                        className="w-[300px] max-h-[300px] overflow-y-auto"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormDescription>This is the date and time that will be converted.</FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="timezone"
+                        render={({ field }) => (
+                            <FormItem className="flex flex-col">
+                                <FormLabel className="mt-4">Timezone</FormLabel>
+                                <Popover>
+                                    <PopoverTrigger asChild>
+                                        <FormControl>
+                                            <Button
+                                                variant="outline"
+                                                role="combobox"
+                                                className={cn(
+                                                    "w-[300px] justify-between",
+                                                    !field.value && "text-muted-foreground"
+                                                )}
+                                            >
+                                                {field.value
+                                                    ? timezones.find((tz) => tz.timezone === field.value)?.timezone
+                                                    : "Select timezone"}
+                                                <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                                            </Button>
+                                        </FormControl>
+                                    </PopoverTrigger>
+                                    <PopoverContent className="w-[300px] max-h-[300px] overflow-y-auto p-0">
+                                        <Command>
+                                            <CommandInput placeholder="Type to search (country, city)" />
+                                            <CommandEmpty>No timezone found.</CommandEmpty>
+                                            <CommandGroup key="default" heading="Default">
+                                                <CommandItem
+                                                    value={localTimezone.timezone}
+                                                    key="default"
+                                                    onSelect={() => {
+                                                        form.setValue("timezone", localTimezone.timezone)
+                                                    }}
+                                                >
+                                                    <Check
+                                                        className={cn(
+                                                            "mr-2 h-4 w-4",
+                                                            localTimezone.timezone === field.value
+                                                                ? "opacity-100"
+                                                                : "opacity-0"
+                                                        )}
+                                                    />
+                                                    <span>{localTimezone.timezone}</span>
+                                                    <CommandShortcut>{localTimezone.group}</CommandShortcut>
+                                                </CommandItem>
+                                                <CommandItem
+                                                    value={utc.timezone}
+                                                    key="default"
+                                                    onSelect={() => {
+                                                        form.setValue("timezone", utc.timezone)
+                                                    }}
+                                                >
+                                                    <Check
+                                                        className={cn(
+                                                            "mr-2 h-4 w-4",
+                                                            utc.timezone === field.value ? "opacity-100" : "opacity-0"
+                                                        )}
+                                                    />
+                                                    <span>{utc.timezone}</span>
+                                                    <CommandShortcut>{utc.group}</CommandShortcut>
+                                                </CommandItem>
+                                            </CommandGroup>
+                                            {groups.map((group) => (
+                                                <CommandGroup key={group} heading={group}>
+                                                    {timezones.map((tz) =>
+                                                        tz.timezone.split("/")[0] === group ? (
+                                                            <CommandItem
+                                                                value={tz.timezone}
+                                                                key={tz.timezone}
+                                                                onSelect={() => {
+                                                                    form.setValue("timezone", tz.timezone)
+                                                                }}
+                                                            >
+                                                                <Check
+                                                                    className={cn(
+                                                                        "mr-2 h-4 w-4",
+                                                                        tz.timezone === field.value
+                                                                            ? "opacity-100"
+                                                                            : "opacity-0"
+                                                                    )}
+                                                                />
+                                                                <span>{tz.timezone}</span>
+                                                                <CommandShortcut>{tz.group}</CommandShortcut>
+                                                            </CommandItem>
+                                                        ) : null
+                                                    )}
+                                                </CommandGroup>
+                                            ))}
+                                        </Command>
+                                    </PopoverContent>
+                                </Popover>
+                                <FormDescription>This is the timezone you want to display.</FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit" className="p-5 my-5 bg-blue-500">
+                        Submit
+                    </Button>
+                </form>
+            </Form>
         </main>
     )
 }
